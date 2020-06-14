768603d763a779976f69479d44be7c400bd6adf6
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-06-14 17:49:44 -0500

    upaloded the course data on the server

--- init-db.ts
+++ init-db.ts
@@ -1,37 +1,38 @@
+import { COURSES, findLessonsForCourse } from "./db-data";
 
-import {COURSES, findLessonsForCourse} from './db-data';
-
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyDAdoefqX5OqjkD3BkW25ZAL6XYZMo4Vz8",
-  authDomain: "fir-course-17549.firebaseapp.com",
-  databaseURL: "https://fir-course-17549.firebaseio.com",
-  projectId: "fir-course-17549",
-  storageBucket: "fir-course-17549.appspot.com",
-  messagingSenderId: "170806523820",
-  appId: "1:170806523820:web:a3181632d54d076a0bec09"
+  apiKey: "AIzaSyDTCGRHA8TtPZOFlZuBhzSer98qvAPMBoc",
+  authDomain: "fir-course-cafee.firebaseapp.com",
+  databaseURL: "https://fir-course-cafee.firebaseio.com",
+  projectId: "fir-course-cafee",
+  storageBucket: "fir-course-cafee.appspot.com",
+  messagingSenderId: "591841429823",
+  appId: "1:591841429823:web:7ada1f100fa1101c42eb8a",
 };
 
 console.log("Uploading data to the database with the following config:\n");
 
 console.log(JSON.stringify(config));
 
-console.log("\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n");
+console.log(
+  "\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n"
+);
 
 const app = firebase.initializeApp(config);
 const db = firebase.firestore();
 
-main().then(r => console.log('Done.'));
+main().then((r) => console.log("Done."));
 
 async function uploadData() {
-  const courses = await db.collection('courses');
+  const courses = await db.collection("courses");
   for (let course of Object.values(COURSES)) {
     const newCourse = removeId(course);
     const courseRef = await courses.add(newCourse);
-    const lessons = await courseRef.collection('lessons');
-    const courseLessons = findLessonsForCourse(course['id']);
-    console.log(`Uploading course ${course['titles']["description"]}`);
+    const lessons = await courseRef.collection("lessons");
+    const courseLessons = findLessonsForCourse(course["id"]);
+    console.log(`Uploading course ${course["titles"]["description"]}`);
     for (const lesson of courseLessons) {
       const newLesson = removeId(lesson);
       await lessons.add(newLesson);
@@ -40,19 +41,18 @@ async function uploadData() {
 }
 
 function removeId(data: any) {
-  const newData: any = {...data};
+  const newData: any = { ...data };
   delete newData.id;
   return newData;
 }
 
-async function main(){
+async function main() {
   try {
-    console.log('Start main...\n\n');
+    console.log("Start main...\n\n");
     await uploadData();
-    console.log('\n\nClosing Application...');
+    console.log("\n\nClosing Application...");
     await app.delete();
-  }catch (e) {
-    console.log('Data upload failed, reason:', e, '\n\n');
+  } catch (e) {
+    console.log("Data upload failed, reason:", e, "\n\n");
   }
 }
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,12 @@
 export const environment = {
   production: false,
   firebase: {
-
-  }
+    apiKey: "AIzaSyDTCGRHA8TtPZOFlZuBhzSer98qvAPMBoc",
+    authDomain: "fir-course-cafee.firebaseapp.com",
+    databaseURL: "https://fir-course-cafee.firebaseio.com",
+    projectId: "fir-course-cafee",
+    storageBucket: "fir-course-cafee.appspot.com",
+    messagingSenderId: "591841429823",
+    appId: "1:591841429823:web:7ada1f100fa1101c42eb8a",
+  },
 };
