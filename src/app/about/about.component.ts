81dace40c43ff3614dffd085526b32a650ee0ce7
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-06-15 09:47:24 -0500

    Started adding CRUD operations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+
+// import * as firebase from "firebase/app";
+// import "firebase/firestore";
+
+// const config = {
+//   apiKey: "AIzaSyDTCGRHA8TtPZOFlZuBhzSer98qvAPMBoc",
+//   authDomain: "fir-course-cafee.firebaseapp.com",
+//   databaseURL: "https://fir-course-cafee.firebaseio.com",
+//   projectId: "fir-course-cafee",
+//   storageBucket: "fir-course-cafee.appspot.com",
+//   messagingSenderId: "591841429823",
+//   appId: "1:591841429823:web:7ada1f100fa1101c42eb8a",
+// };
+
+// firebase.initializeApp(config);
+
+// const db = firebase.firestore();
+// db.settings({ timestampsInSnapshots: true });
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
+    this.db
+      .collection("courses")
+      .stateChanges()
+      .subscribe((snaps) => {
+        console.log(snaps);
+      });
   }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
-
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private db: AngularFirestore) {}
+  courses$: Observable<Course[]>;
 
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+  ngOnInit() {
+    this.courses$ = this.db
+      .collection("courses")
+      .snapshotChanges()
+      .pipe(map((snaps) => this.convertSnaps<Course>(snaps)));
 
-    constructor() {
+    this.beginnerCourses$ = this.courses$.pipe(
+      map((courses) => {
+        return courses.filter((course) =>
+          course.categories.includes("BEGINNER")
+        );
+      })
+    );
 
-    }
+    this.advancedCourses$ = this.courses$.pipe(
+      map((courses) => {
+        return courses.filter((course) =>
+          course.categories.includes("ADVANCED")
+        );
+      })
+    );
+  }
 
-    ngOnInit() {
-
-
-
-    }
-
+  convertSnaps<T>(snaps): T[] {
+    return <T[]>snaps.map((snap) => {
+      return {
+        id: snap.payload.doc.id,
+        ...(snap.payload.doc.data() as object),
+      };
+    });
+  }
 }
